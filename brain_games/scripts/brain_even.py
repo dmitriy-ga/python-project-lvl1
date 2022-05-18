6404cfb34ca47cd2ac90182c123d7b06f91f69ab
dmitriy-ga <flash2243@gmail.com>
2022-05-18 13:55:52 +0500

    fixed brain_even.py for E501 and some readability in parts

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,19 +6,25 @@ def main():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+
     print('Answer "yes" if the number is even, otherwise answer "no".')
     for i in range(3):
         question = randint(1, 100)
         print(f'Question: {question}')
+
         answer = prompt.string('Your answer: ').lower().strip()
         right_answer = 'yes' if question % 2 == 0 else 'no'
+
         if answer == right_answer:
             print('Correct!')
             continue
         else:
-            print(f"'{answer}' is wrong answer. Correct answer was '{right_answer}'.")
+            print(f"'{answer}' is wrong answer. "
+                  f"Correct answer was '{right_answer}'.")
+            # This print in two lines printing on one line
             print(f"Let's try again, {name}!")
             exit()
+
     print(f'Congratulations, {name}!')
 
 
