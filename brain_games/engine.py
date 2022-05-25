be5ee3dd7d1ce6fc74ae148da7e7ef446c6a7a47
dmitriy-ga <flash2243@gmail.com>
2022-05-25 08:34:07 +0500

    common_games_funcs.py mergered with engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,7 +1,26 @@
 from brain_games.games import *  # noqa: F401, F403
-from brain_games.common_games_funcs import greeting, checking_answer
+import prompt
 
 ROUNDS = 3
+
+
+def checking_answer(answer, right_answer, name):
+    if answer == right_answer:
+        print('Correct!')
+    else:
+        # Game over, exit
+        print(f"'{answer}' is wrong answer. "
+              f"Correct answer was '{right_answer}'.")
+        # This print in two lines printing on one line
+        print(f"Let's try again, {name}!")
+        exit()
+
+
+def greeting():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
 
 
 def main(game, task):
