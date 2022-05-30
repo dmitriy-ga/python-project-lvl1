f8cb58a2eb632e4a10477ca04ab518a1b54d25d4
dmitriy-ga <flash2243@gmail.com>
2022-05-30 10:13:12 +0500

    Number generation moved outside of operation check

--- brain_games/games/logic_calc.py
+++ brain_games/games/logic_calc.py
@@ -6,19 +6,15 @@ MAX_NUMBER = 100
 
 def gaming():
     operation = choice(['+', '-', '*'])
+    number1 = randint(MIN_NUMBER, MAX_NUMBER)
+    number2 = randint(MIN_NUMBER, MAX_NUMBER)
     if operation == '-':
-        number1 = randint(MIN_NUMBER, MAX_NUMBER)
-        number2 = randint(MIN_NUMBER, MAX_NUMBER)
         question = f'{number1} - {number2}'
         right_answer = number1 - number2
     elif operation == '*':
-        number1 = randint(MIN_NUMBER, MAX_NUMBER)
-        number2 = randint(MIN_NUMBER, MAX_NUMBER)
         question = f'{number1} * {number2}'
         right_answer = number1 * number2
     else:
-        number1 = randint(MIN_NUMBER, MAX_NUMBER)
-        number2 = randint(MIN_NUMBER, MAX_NUMBER)
         question = f'{number1} + {number2}'
         right_answer = number1 + number2
     return question, right_answer
