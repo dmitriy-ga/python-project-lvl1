1bd3ad692b693c4ff212343c5879301adccdff3a
dmitriy-ga <flash2243@gmail.com>
2022-05-30 10:36:12 +0500

    simplification of answer checking

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,15 +3,6 @@ import prompt
 ROUNDS_COUNT = 3
 
 
-def checking_answer(answer, right_answer):
-    if str(answer) == str(right_answer):
-        print('Correct!')
-        winning = True
-    else:
-        winning = False
-    return winning
-
-
 def greeting():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
@@ -22,18 +13,19 @@ def greeting():
 def gaming(game, task):
     name = greeting()
     print(task)
-    winning = None
     for i in range(ROUNDS_COUNT):
         question, right_answer = game()
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ').lower()
-        winning = checking_answer(answer, right_answer)
-        if not winning:
+        if str(answer) == str(right_answer):
+            print('Correct!')
+        else:
             # Game over, exit
             print(f"'{answer}' is wrong answer. "
                   f"Correct answer was '{right_answer}'.")
             # This print in two lines printing on one line
             print(f"Let's try again, {name}!")
             break
-    if winning:
+    else:
+        # Exited from 'for' without 'break'
         print(f'Congratulations, {name}!')
